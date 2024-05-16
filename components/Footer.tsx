1c4f32550eaedd3ee0f0278bf27f50aeb779889c
Sakuya020 <42985269+Sakuya020@users.noreply.github.com>
2024-05-16 12:32:15 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,14 +5,14 @@ const Footer = () => {
       <div className="hidden sm:block">
         <div className="h-8"></div>
         <div className="fixed bottom-0 flex items-center h-8 w-[calc(100vw-20px)] border-t border-foreground bg-background">
-          <p>© Joyce Shi 2016-2024</p>
+          <p>© Joyce Shi 2018-2024</p>
         </div>
       </div>
       {/* footer for phone view */}
       <div className="sm:hidden">
         <div className="h-[63px]"></div>
         <div className="fixed bottom-0 flex pt-[15px] h-16 w-[calc(100vw-32px)] bg-background border-t border-foreground">
-          <p>© Joyce Shi 2016-2024</p>
+          <p>© Joyce Shi 2018-2024</p>
         </div>
       </div>
     </footer>
