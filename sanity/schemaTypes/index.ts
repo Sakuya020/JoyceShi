75ee938368446d6188090b36081c8828bb62ba56
Sakuya020 <42985269+Sakuya020@users.noreply.github.com>
2024-10-12 13:23:05 +0900

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import Introduction from './Introduction'
 import portfolio from './portfolio'
+import archived from './archived'
 
-export const schemaTypes = [portfolio, Introduction]
+export const schemaTypes = [portfolio, Introduction, archived]
