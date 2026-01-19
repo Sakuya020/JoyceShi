6db0e15738284cd1c6f2d3ef8ea038189d8acb2a
Sakuya020 <42985269+Sakuya020@users.noreply.github.com>
2026-01-18 20:19:13 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,7 @@ export const metadata: Metadata = {
   title: "Joyce Shi",
   description:
     "Joyce Shi is a concept-driven, award-winning design director based in New York.
-
-She is the founder of publication practice G Axis Press and design studio Function Lab, with work featured globally in bookstores, galleries, and institutions including the Thomas J. Watson Library at the MET.
+    She is the founder of publication practice G Axis Press and design studio Function Lab, with work featured globally in bookstores, galleries, and institutions including the Thomas J. Watson Library at the MET.
   ",
   icons: {
     icon: "/icon.svg",
